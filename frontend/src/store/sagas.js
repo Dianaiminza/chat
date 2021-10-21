fd985733e5a6c4810c99e8d8b8f6267d0eb93b6e
Dianaiminza <dianaiminza.99@gmail.com>
2021-10-21 22:45:04 +0300

    my first commit

---
+++ frontend/src/store/sagas.js
@@ -0,0 +1,12 @@
+import { all } from 'redux-saga/effects';
+
+import workspaceList from '../routes/WorkspaceList/modules/sagas';
+import workspaceApp from '../wrappers/WorkspaceApp/modules/sagas';
+
+export default function* rootSaga() {
+  yield all([
+    workspaceList(),
+    workspaceApp(),
+  ]);
+}
+© 2021 GitHub, Inc.
